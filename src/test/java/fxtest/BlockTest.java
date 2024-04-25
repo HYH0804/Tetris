b5267e1483887003d9506a4cc56e425029d90787
5512rose@naver.com <guswnsgur2276@gmail.com>
2024-04-26 02:06:49 +0900

    59퍼센트_4/26

--- src/test/java/fxtest/BlockTest.java
+++ src/test/java/fxtest/BlockTest.java
@@ -48,4 +48,124 @@ public class BlockTest {
 
     }
 
+    @Test
+    void setterJ(){
+        BrickJ brickJ = new BrickJ(1, 4, Color.BLACK);
+        Block blockA = new Block(5, 5, Item.ROWDELETE, Color.BLACK);
+        Block blockB = new Block(5, 5, Item.ROWDELETE, Color.BLACK);
+        Block blockC = new Block(5, 5, Item.ROWDELETE, Color.BLACK);
+        Block blockD = new Block(5, 5, Item.ROWDELETE, Color.BLACK);
+
+        brickJ.setA(blockA);
+        brickJ.setB(blockB);
+        brickJ.setC(blockC);
+        brickJ.setD(blockD);
+
+        Assertions.assertEquals(brickJ.getA().getX(), 5);
+        Assertions.assertEquals(brickJ.getB().getX(), 5);
+        Assertions.assertEquals(brickJ.getC().getX(), 5);
+        Assertions.assertEquals(brickJ.getD().getX(), 5);
+
+    }
+
+    @Test
+    void setterL(){
+        BrickL brickL = new BrickL(1, 4, Color.BLACK);
+        Block blockA = new Block(5, 5, Item.ROWDELETE, Color.BLACK);
+        Block blockB = new Block(5, 5, Item.ROWDELETE, Color.BLACK);
+        Block blockC = new Block(5, 5, Item.ROWDELETE, Color.BLACK);
+        Block blockD = new Block(5, 5, Item.ROWDELETE, Color.BLACK);
+
+        brickL.setA(blockA);
+        brickL.setB(blockB);
+        brickL.setC(blockC);
+        brickL.setD(blockD);
+
+        Assertions.assertEquals(brickL.getA().getX(), 5);
+        Assertions.assertEquals(brickL.getB().getX(), 5);
+        Assertions.assertEquals(brickL.getC().getX(), 5);
+        Assertions.assertEquals(brickL.getD().getX(), 5);
+
+    }
+
+    @Test
+    void setterO(){
+        BrickO brickO = new BrickO(1, 4, Color.BLACK);
+        Block blockA = new Block(5, 5, Item.ROWDELETE, Color.BLACK);
+        Block blockB = new Block(5, 5, Item.ROWDELETE, Color.BLACK);
+        Block blockC = new Block(5, 5, Item.ROWDELETE, Color.BLACK);
+        Block blockD = new Block(5, 5, Item.ROWDELETE, Color.BLACK);
+
+        brickO.setA(blockA);
+        brickO.setB(blockB);
+        brickO.setC(blockC);
+        brickO.setD(blockD);
+
+        Assertions.assertEquals(brickO.getA().getX(), 5);
+        Assertions.assertEquals(brickO.getB().getX(), 5);
+        Assertions.assertEquals(brickO.getC().getX(), 5);
+        Assertions.assertEquals(brickO.getD().getX(), 5);
+
+    }
+
+    @Test
+    void setterS(){
+        BrickS brickS = new BrickS(1, 4, Color.BLACK);
+        Block blockA = new Block(5, 5, Item.ROWDELETE, Color.BLACK);
+        Block blockB = new Block(5, 5, Item.ROWDELETE, Color.BLACK);
+        Block blockC = new Block(5, 5, Item.ROWDELETE, Color.BLACK);
+        Block blockD = new Block(5, 5, Item.ROWDELETE, Color.BLACK);
+
+        brickS.setA(blockA);
+        brickS.setB(blockB);
+        brickS.setC(blockC);
+        brickS.setD(blockD);
+
+        Assertions.assertEquals(brickS.getA().getX(), 5);
+        Assertions.assertEquals(brickS.getB().getX(), 5);
+        Assertions.assertEquals(brickS.getC().getX(), 5);
+        Assertions.assertEquals(brickS.getD().getX(), 5);
+
+    }
+
+    @Test
+    void setterT(){
+        BrickT brickT = new BrickT(1, 4, Color.BLACK);
+        Block blockA = new Block(5, 5, Item.ROWDELETE, Color.BLACK);
+        Block blockB = new Block(5, 5, Item.ROWDELETE, Color.BLACK);
+        Block blockC = new Block(5, 5, Item.ROWDELETE, Color.BLACK);
+        Block blockD = new Block(5, 5, Item.ROWDELETE, Color.BLACK);
+
+        brickT.setA(blockA);
+        brickT.setB(blockB);
+        brickT.setC(blockC);
+        brickT.setD(blockD);
+
+        Assertions.assertEquals(brickT.getA().getX(), 5);
+        Assertions.assertEquals(brickT.getB().getX(), 5);
+        Assertions.assertEquals(brickT.getC().getX(), 5);
+        Assertions.assertEquals(brickT.getD().getX(), 5);
+
+    }
+
+    @Test
+    void setterZ(){
+        BrickZ brickZ = new BrickZ(1, 4, Color.BLACK);
+        Block blockA = new Block(5, 5, Item.ROWDELETE, Color.BLACK);
+        Block blockB = new Block(5, 5, Item.ROWDELETE, Color.BLACK);
+        Block blockC = new Block(5, 5, Item.ROWDELETE, Color.BLACK);
+        Block blockD = new Block(5, 5, Item.ROWDELETE, Color.BLACK);
+
+        brickZ.setA(blockA);
+        brickZ.setB(blockB);
+        brickZ.setC(blockC);
+        brickZ.setD(blockD);
+
+        Assertions.assertEquals(brickZ.getA().getX(), 5);
+        Assertions.assertEquals(brickZ.getB().getX(), 5);
+        Assertions.assertEquals(brickZ.getC().getX(), 5);
+        Assertions.assertEquals(brickZ.getD().getX(), 5);
+
+    }
+
 }
