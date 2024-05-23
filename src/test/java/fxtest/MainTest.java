b036e315a0041d12dcea53389c950484be320ac2
bogil SO <sotomas232@gmail.com>
2024-05-23 10:25:23 +0900

    S
    Signed-off-by: bogil SO <sotomas232@gmail.com>

--- src/test/java/fxtest/MainTest.java
+++ src/test/java/fxtest/MainTest.java
@@ -9,15 +9,4 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
-    @Test
-    void loadProperties() throws IOException {
-        boolean test =true;
-        if(Main.loadProperties()!=null){
-            test = true;
-        }
-        else {
-            test = false;
-        }
-        assertTrue(test);
-    }
 }
